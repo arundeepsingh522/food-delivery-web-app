5376f7c0e379c6e3723cfeaf8737831544080874
Arundeep Singh <arundeepsingh522@gmail.com>
2024-04-24 21:47:30 -0700

    25april1020am

--- frontend/scripts/registration.js
+++ frontend/scripts/registration.js
@@ -1,3 +1,43 @@
+const userInput=document.getElementById('username');
+const passwordInput=document.getElementById('password');
+const emailInput = document.getElementById('email');
+const confirmInput = document.getElementById('confirm-password');
+const errorMessage=document.getElementById('errormessage');
+
+username.addEventListener('input',(event)=>{
+   if(event.target.value.length<6)
+   {
+    errorMessage.innerText="Username must be at least 6 characters long";
+   }else{
+    errorMessage.innerText="";
+   }
+});
+passwordInput.addEventListener('input',(event)=>{
+    if(!(isValidPassword(event.target.value)))
+    {
+        errorMessage.innerText="Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long.";
+
+    }else{
+        errorMessage.innerText="";
+    }
+
+});
+emailInput.addEventListener('focus',()=>{
+errorMessage.innerText="";
+});
+confirmInput.addEventListener('focus',()=>{
+    errorMessage.innerText="";
+
+});
+userInput.addEventListener('focus',()=>{
+    errorMessage.innerText="";
+
+});
+passwordInput.addEventListener('focus',()=>{
+    errorMessage.innerText=""
+});
+
+//on click submit
 document.querySelector('#submit-signup').addEventListener('click',(event)=>{
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
@@ -23,9 +63,13 @@ document.querySelector('#submit-signup').addEventListener('click',(event)=>{
     {
         alert(errorMessage);
     }else{
-        checkValidations(username,email,password,confirmPassword)
+        checkValidations(username,email,password,confirmPassword);
     }
     
+  
+    }
+    );
+
     function checkValidations(username,email,password,confirmPassword)
     {
        
@@ -53,7 +97,7 @@ document.querySelector('#submit-signup').addEventListener('click',(event)=>{
         }else{
             //all conditions are satisfied
             //now go to the home section and also save the user
-            let saveUser= saveUser();
+            //let saveUser= saveUser();
             alert("all satisfied");
         }
     
@@ -69,6 +113,4 @@ document.querySelector('#submit-signup').addEventListener('click',(event)=>{
       const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
       return pattern.test(password);
     }
-    }
-    )
     
